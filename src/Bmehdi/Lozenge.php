461cfb90507f8ddc7342045fda312fc007f38ab2
robinduval <leyoda@gmail.com>
2016-12-09 16:18:08 +0100

    Exercice Mehdi

--- src/Bmehdi/Lozenge.php
+++ src/Bmehdi/Lozenge.php
@@ -5,6 +5,7 @@ namespace Hackathon3\Bmehdi;
 class Lozenge
 {
     private $size;
+    public $char = 'o';
 
     public function __construct($size)
     {
@@ -45,9 +46,29 @@ class Lozenge
     public function __toString()
     {
         $lozenge = '';
+        $height = $this->size + 1;
+        $size = $this->size;
 
-        // todo : update $lozenge
+        for ($i = 0; $i < $height; $i++) {
+            $comp = $size - $i;
+            $lozenge = $lozenge . str_repeat(' ', $comp) . $this->char;
+            if ($i != 0) {
+                $space = (1 === $i) ? 1 : ($i * 2 - 1);
+                $lozenge = $lozenge . str_repeat(' ', $space) . $this->char;
+            }
+            $lozenge = $lozenge . str_repeat(' ', $comp) . PHP_EOL;
+        }
+        
+        for ($i = $height - 2; $i >= 0; $i--) {
+            $comp = $size - $i;
+            $lozenge = $lozenge . str_repeat(' ', $comp) . $this->char;
+            if ($i != 0) {
+                $space = (1 === $i) ? 1 : ($i * 2 - 1);
+                $lozenge = $lozenge . str_repeat(' ', $space) . $this->char;
+            }
+            $lozenge = $lozenge . str_repeat(' ', $comp) . PHP_EOL;
+        }
 
-        return $lozenge;
+        return substr($lozenge, 0, -1);
     }
 }
